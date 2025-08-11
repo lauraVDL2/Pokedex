e1de2accee94d01fa1a0b6b433f5cfe0547eb0d0
lauraVDL2 <laura.vanderderlinden@hotmail.com>
2025-08-11 17:57:44 +0200

    fix evolution chart bug

--- frontend/src/app/core/evolution-chart.service.ts
+++ frontend/src/app/core/evolution-chart.service.ts
@@ -10,12 +10,15 @@ export class EvolutionChartService {
 
   sortEvolutionChart(currentPokemon: Pokemon, evolvedStages: Pokemon[]): Map<number, Pokemon> {
     const map = new Map();
+    // Current is last Pokemon of its line
     if(!currentPokemon?.evolutionStageEntry?.next) {
       map.set(evolvedStages.length, currentPokemon);
+      // Case of a line of two
       if(evolvedStages.length == 1) {
         map.set(0, evolvedStages[0]);
       }
       else {
+        // Order of evolutions
         if(!evolvedStages[0]?.evolutionStageEntry?.previous) {
           map.set(0, evolvedStages[0]);
           map.set(1, evolvedStages[1]);
@@ -26,12 +29,15 @@ export class EvolutionChartService {
         }
       }
     }
+    // Current is first Pokemon of its line
     else if(!currentPokemon?.evolutionStageEntry?.previous) {
       map.set(0, currentPokemon);
+      // Case of a line of two
       if(evolvedStages.length == 1) {
         map.set(1, evolvedStages[0]);
       }
       else {
+        // Order of evolutions
         if (evolvedStages[0]?.evolutionStageEntry?.next?.length == 1) {
           if(!evolvedStages[0]?.evolutionStageEntry?.next?.at(0)) {
             map.set(2, evolvedStages[0]);
@@ -42,6 +48,7 @@ export class EvolutionChartService {
             map.set(1, evolvedStages[0]);
           }
         }
+        // Eevee
         else {
           for(let i = 0; i < evolvedStages.length; i++) {
             map.set(i + 1, evolvedStages[i]);
@@ -52,25 +59,22 @@ export class EvolutionChartService {
     // Middle evolution stage (always in line of three in this case)
     else {
       map.set(1, currentPokemon);
-      if (evolvedStages[0]?.evolutionStageEntry?.next?.length == 1) {
-        if (!evolvedStages[0]?.evolutionStageEntry?.next?.at(0)) {
-          map.set(2, evolvedStages[0]);
-          map.set(0, evolvedStages[1]);
-        }
-        else {
-          map.set(2, evolvedStages[1]);
-          map.set(0, evolvedStages[0]);
-        }
+      if (!evolvedStages[0]?.evolutionStageEntry?.next?.at(0)) {
+        map.set(2, evolvedStages[0]);
+        map.set(0, evolvedStages[1]);
       }
       else {
-          for(let i = 0; i < evolvedStages.length; i++) {
-            map.set(i + 1, evolvedStages[i]);
-          }
+        map.set(2, evolvedStages[1]);
+        map.set(0, evolvedStages[0]);
+      }
+      // In case of multiple evolutions possibles, we don't erase what has been done before
+      if (evolvedStages[0]?.evolutionStageEntry?.next?.length && 
+      evolvedStages[0]?.evolutionStageEntry?.next?.length > 1) {
+        for(let i = 2; i < evolvedStages.length; i++) {
+          map.set(i + 1, evolvedStages[i]);
         }
+      }
     }
-    // Evee
-   
-      console.log(evolvedStages);
     return new Map([...map.entries()].sort());
   }
 
